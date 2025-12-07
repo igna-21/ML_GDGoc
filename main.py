4776c43eca4198f388061a76808a09ca34b4e308
ignatius <ignatiusjuan2@gmail.com>
2025-12-07 19:11:17 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-file = 'Study jam 1\dengan data sudah ditentukan\data.txt'
+import random
+
+file = 'Study jam 1\dengan library random\data.txt'
+
+with open(file, "w") as f:
+    pass
+with open(file, "w") as f:
+    for i in range(20):
+        random_integer = random.randint(1, 20)
+        f.write(str(random_integer))
+        f.write("\n")
+f.close()
 
 with open(file, 'r') as file:
     lines_list = file.readlines()
